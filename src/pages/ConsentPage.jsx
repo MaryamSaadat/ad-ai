b04b5191ab02e471818d92bfca2ab71c3ad29d1a
maryamcheema <22100276@lums.edu.pk>
2024-04-07 19:43:48 +0300

    fixed the consent page

--- src/pages/ConsentPage.jsx
+++ src/pages/ConsentPage.jsx
@@ -61,12 +61,16 @@ const ConsentPage = () => {
           We do not anticipate any risks to you from this study. There are no
           anticipated direct benefits to you from participating in the study
           alone. Your participation in this study is voluntary. You can take a
-          break or discontinue participation at any time. We will send you a $20
+          break or discontinue participation at any time. We will send you a $50
           Amazon gift card as a token of appreciation for completing the study.
           Please note that we might need a few days to send you the gift cards
           because we order them in batches.
           <br />
-          We will not record any identifying information including names,
+          We will ask a few questions related to the videos watched in the
+          viewing segment and some demographic data. We will audio record the
+          session and take notes to record your comments. We will not request,
+          and you should not provide, any sensitive personal information in this
+          study. We will not record any identifying information including names,
           emails, etc. without your permission. The data collected may be used
           to analyze and improve the audio descriptions for our research
           project. By giving your consent, you agree to allow us to use all data
